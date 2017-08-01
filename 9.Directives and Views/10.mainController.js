a22508da30af296759d45724ff1f35c27d0b24e8
Archie <liteshotv3@gmail.com>
2017-08-01 00:34:21 -0700

    implemented search function in 10.ng-model.html and 1-=0.mainController.js removed unnecessary and broken code

--- 9.Directives and Views/10.mainController.js
+++ 9.Directives and Views/10.mainController.js
@@ -1,28 +1,25 @@
 var app = angular.module('myApp', []); //'myApp is the name of the module and dependencies go inside the [] if there are any
-app.controller('MainController', function($scope, $http)
-{
+app.controller('MainController', function ($scope, $http) {
     //I want to use this in my then function on line 12, but I don't want to clutter code, so I will
     //define it here and simply pass it as the then() parameter
-    var onUserComplete = function(response){
+    var onUserComplete = function (response) {
         $scope.user = response.data;
     };
 
-    var onError = function(reason){
+    var onError = function (reason) {
         $scope.error = "Could not fetch the user for some reason";
     };
 
-    var username = 'liteshotv3'
-
-    $http.get("https://api.github.com/users/"+username)
+    $http.get("https://api.github.com/users/angular")
         .then(onUserComplete, onError);
-    var person = {
-        firstName: "Archie",
-        lastName: "Tolstov",
-        //imageSrc: "https://pbs.twimg.com/profile_images/2824227101/20823644bf8866ab3454efd4ccda40f8.jpeg"
-    };
+
+    $scope.search = function (username) {
+        $http.get("https://api.github.com/users/" + username)
+            .then(onUserComplete, onError);
+    }
+
 
     $scope.message = "Github viewer";
-    $scope.person = person;
     $scope.username;
 
 });
